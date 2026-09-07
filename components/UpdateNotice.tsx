221528dfcf24f2d6f0697699756e48f77a20d431
Divide-By-0 <aayushg@mit.edu>
2026-09-07 00:16:57 -0700

    Remove unused import from disabled update notice

--- components/UpdateNotice.tsx
+++ components/UpdateNotice.tsx
@@ -1,4 +1,3 @@
-import { contactEmail } from '../lib/contact'
 import * as React from 'react';
 import styles from './styles.module.css';
 
@@ -10,7 +9,7 @@ export const UpdateNotice: React.FC = () => {
           <span className={styles.cursive}>🌱 We are always updating Coursetexts.</span>
         </h1>
         <p>
-          These notes are drafts and meant to be more up-to-date than they are polished. If you spot a typo, please let us know at {contactEmail}!
+          These notes are drafts and meant to be more up-to-date than they are polished. Please use the site's contact link to report a typo.
         </p>
       </div>
       <button className={styles.button}>Learn more →</button> */}
